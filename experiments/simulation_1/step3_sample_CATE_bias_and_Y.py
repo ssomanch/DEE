1dcfd21070991546a01296ce82a7badeb9414a0e
ssomanch <ssomanch@mcobfe.crc.nd.edu>
2022-12-07 21:41:37 -0500

    Improving memory utilization by writing function for generating CATE GP and Bias GP rather than global variables

--- experiments/simulation_1/step3_sample_CATE_bias_and_Y.py
+++ experiments/simulation_1/step3_sample_CATE_bias_and_Y.py
@@ -8,6 +8,10 @@ from gpytorch.kernels.scale_kernel import ScaleKernel
 
 import argparse
 
+import gc
+import copy
+
+
 class ExactGPModel(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, likelihood):
         super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
@@ -19,6 +23,64 @@ class ExactGPModel(gpytorch.models.ExactGP):
         covar_x = self.covar_module(x)
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
+def generate_GPs(args, X, Y, all_Xs):
+    
+    ########################################
+    # Sample the CATE                      #
+    ########################################
+
+    hypers = {
+        'likelihood.noise_covar.noise': torch.tensor(1.),
+        'covar_module.base_kernel.lengthscale': torch.tensor(float(args.CATE_ls)),
+        'covar_module.outputscale': torch.tensor(5.),
+    }
+
+    likelihood = gpytorch.likelihoods.GaussianLikelihood()
+
+    gp = ExactGPModel(X,Y,likelihood)
+    gp.initialize(**hypers)
+
+    gp.eval()
+    likelihood.eval()
+
+    with gpytorch.settings.prior_mode(True):
+        prior_sample = gp(all_Xs)
+
+    rsample = prior_sample.rsample()
+
+    rsample_numpy = copy.deepcopy(rsample.detach().numpy())
+   
+    del gp
+    del likelihood
+    del rsample
+    del prior_sample
+
+    gc.collect(generation=2)
+
+    ########################################
+    # Sample the bias                      #
+    ########################################
+
+    hypers = {
+        'likelihood.noise_covar.noise': torch.tensor(1.),
+        'covar_module.base_kernel.lengthscale': torch.tensor(float(args.bias_ls)),
+        'covar_module.outputscale': torch.tensor(5.),
+    }
+
+    likelihood = gpytorch.likelihoods.GaussianLikelihood()
+    gp = ExactGPModel(X,Y,likelihood)
+    gp.initialize(**hypers)
+
+    gp.eval()
+    likelihood.eval()
+
+    with gpytorch.settings.prior_mode(True):
+        prior_sample = gp(all_Xs)
+
+    rsample_bias = prior_sample.rsample()
+    rsample_bias_numpy = copy.deepcopy(rsample_bias.detach().numpy())
+    
+    return rsample_numpy, rsample_bias_numpy
 
 parser = argparse.ArgumentParser(description='Sample CATE, bias and Y')
 parser.add_argument('--seed1', help='Seed for sampling',type=int)
@@ -54,65 +116,24 @@ U_x = torch.from_numpy(U[['X1','X2']].values)
 # Stack the training instances, test grid, and Voroni centers
 all_Xs = torch.cat([X,test_x,U_x],0)
 all_Xs = all_Xs.type(torch.FloatTensor)
-########################################
-# Sample the CATE                      #
-########################################
 
-hypers = {
-    'likelihood.noise_covar.noise': torch.tensor(1.),
-    'covar_module.base_kernel.lengthscale': torch.tensor(float(args.CATE_ls)),
-    'covar_module.outputscale': torch.tensor(5.),
-}
+rsample_numpy, rsample_bias_numpy = generate_GPs(args, X, Y, all_Xs)
 
-likelihood = gpytorch.likelihoods.GaussianLikelihood()
+train_CATE = rsample_numpy[:N,]
+test_CATE = rsample_numpy[N:(N+test_x.shape[0]),]
+U_CATE = rsample_numpy[(N+test_x.shape[0]):,]
 
-gp = ExactGPModel(X,Y,likelihood)
-gp.initialize(**hypers)
+train_bias = rsample_bias_numpy[:N,]
+test_bias = rsample_bias_numpy[N:(N+test_x.shape[0]),]
+U_bias = rsample_bias_numpy[(N+test_x.shape[0]):,]
 
-gp.eval()
-likelihood.eval()
 
-with gpytorch.settings.prior_mode(True):
-    prior_sample = gp(all_Xs)
-
-rsample = prior_sample.rsample()
-
-train_CATE = rsample[:N,]
-test_CATE = rsample[N:(N+test_x.shape[0]),]
-U_CATE = rsample[(N+test_x.shape[0]):,]
-
-########################################
-# Sample the bias                      #
-########################################
-
-hypers = {
-    'likelihood.noise_covar.noise': torch.tensor(1.),
-    'covar_module.base_kernel.lengthscale': torch.tensor(float(args.bias_ls)),
-    'covar_module.outputscale': torch.tensor(5.),
-}
-
-likelihood = gpytorch.likelihoods.GaussianLikelihood()
-gp = ExactGPModel(X,Y,likelihood)
-gp.initialize(**hypers)
-
-gp.eval()
-likelihood.eval()
-
-with gpytorch.settings.prior_mode(True):
-    prior_sample = gp(all_Xs)
-
-rsample_bias = prior_sample.rsample()
-
-train_bias = rsample_bias[:N,]
-test_bias = rsample_bias[N:(N+test_x.shape[0]),]
-U_bias = rsample_bias[(N+test_x.shape[0]):,]
-
-pd.DataFrame({'CATE':U_CATE.detach().numpy(),
-              'bias':U_bias.detach().numpy()})\
+pd.DataFrame({'CATE':U_CATE,
+              'bias':U_bias})\
   .to_csv(f'{OUTDIR}/true_CATE_and_bias_at_voroni_KNN_centers.csv',index=False)
 
-pd.DataFrame({'CATE':test_CATE.detach().numpy(),
-              'bias':test_bias.detach().numpy()})\
+pd.DataFrame({'CATE':test_CATE,
+              'bias':test_bias})\
   .to_csv(f'{OUTDIR}/true_test_CATE_and_bias.csv',index=False)
 
 ########################################
@@ -127,20 +148,20 @@ c1 = 0*train_bias
 c2 = -4/5*train_bias
 n = -13/10*train_bias
 
-Y0 = -1/2*train_CATE
-Y1 = 1/2*train_CATE
+Y0 = torch.from_numpy(-1/2*train_CATE)
+Y1 = torch.from_numpy(1/2*train_CATE)
 
-group_shift = torch.from_numpy(G=='a')*a + \
-              torch.from_numpy(G=='c1')*c1 + \
-              torch.from_numpy(G=='c2')*c2 + \
-              torch.from_numpy(G=='n')*n
+group_shift = (G=='a')*a + \
+              (G=='c1')*c1 + \
+              (G=='c2')*c2 + \
+              (G=='n')*n
 
-Y = Y1*D + Y0*(~D) + group_shift
+Y = Y1*D + Y0*(~D) + torch.from_numpy(group_shift)
 Y = Y + torch.randn_like(Y)         # Assuming standard normal outcome noise
 
 # Rewrite out csv for downstream
 df['Y'] = Y.detach().numpy()
-df['CATE'] = train_CATE.detach().numpy()
-df['bias'] = train_bias.detach().numpy()
+df['CATE'] = train_CATE
+df['bias'] = train_bias
 
 df.to_csv(f'{OUTDIR}/LORD3_inputs_and_CATE_bias_Y.csv',index=False)
