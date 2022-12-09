d8b89e52751af94f0a01178e6543cea6531b16ac
ssomanch <ssomanch@mcobfe.crc.nd.edu>
2022-12-09 14:29:50 -0500

    Minor bug fix for R plots in rural roads

--- experiments/rural_roads/make_main_R_figures.R
+++ experiments/rural_roads/make_main_R_figures.R
@@ -18,11 +18,12 @@ top_M_nvec = as.matrix(top_M_nvec)
 top_M_nvec = top_M_nvec / sqrt(rowSums(top_M_nvec^2))
 
 abs_cos_sim = abs(apply(top_M_nvec,1,function(r) cosine(r,target_v)))
-qplot(abs_cos_sim) + ylab('Number of discovered local RDs') +
+p <- qplot(abs_cos_sim) + ylab('Number of discovered local RDs') +
   geom_vline(xintercept = cos_pop_thresh,linetype=2,color='red') + 
   xlab('|Cos. similarity| between RD normal vector and population std. basis vector') +
-  ggtitle('Categorizing discovered RD based on RD normal vector') +
-  ggsave('output/rural_roads/figures/cos_sim_plot.png',width=7,height=4)
+  ggtitle('Categorizing discovered RD based on RD normal vector')
+
+ggsave('output/rural_roads/figures/cos_sim_plot.png',p, width=7,height=4)
                    
 nvecs = bound[,c("longitude_nvec","latitude_nvec","total_pop_nvec")]
 nvecs = as.matrix(nvecs)
@@ -33,10 +34,11 @@ bound[['RD type']] = ifelse(pop_RDD,'Population','Spatial')
 
 hline = bound[1500,'LLR']
 
-ggplot(bound[bound$LLR>0,],aes(x=total_pop,y=LLR,color=`RD type`)) + geom_point(size=1,alpha=0.5) + 
+p <- ggplot(bound[bound$LLR>0,],aes(x=total_pop,y=LLR,color=`RD type`)) + geom_point(size=1,alpha=0.5) + 
   xlab('Village population') + ylab('LLR') + geom_hline(yintercept=hline,linetype=2,color='red') + 
-  ggtitle('LORD3 LLR statistic by village population') + 
-  ggsave('output/rural_roads/figures/LLR_plot.png',width=7,height=4)
+  ggtitle('LORD3 LLR statistic by village population') 
+
+ggsave('output/rural_roads/figures/LLR_plot.png',p, width=7,height=4)
                         
 top_M = bound[1:M_prime,]
 fwrite(top_M,'output/rural_roads/figures/top_M_for_map.csv')
